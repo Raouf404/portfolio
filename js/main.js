2a9e148716b1630f374a8c10ab5fca9caa7662d5
Raouf404 <roaufdz222@gmail.com>
2024-12-17 18:52:29 +0100

    FEAT: Added dark theme

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// Variable declaration
+const theme = document.getElementById("theme");
+
+// Function to change the theme
+function changeTheme() {
+  document.body.classList.toggle("dark");
+  theme.children[0].classList.toggle("hide");
+  theme.children[1].classList.toggle("hide");
+}
+
+// Event listener
+theme.addEventListener("click", changeTheme);
